3a442f2cb83a057087b16d5bef7d8ff1250f7bfa
Punith1117 <punith11172006@gmail.com>
2025-05-11 15:22:38 +0530

    add code to add new user with sign-up form, handle unexpected errors and modify code to use req.body
    'extended: true' enables us to handle complex data sent and not just key-value pairs

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express')
 require('dotenv').config()
+const bcrypt = require('bcryptjs')
+const db = require('./db/queries')
 const path = require('node:path')
 
 const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: true}))
 
-app.post('/sign-up', (req, res) => {
-    res.redirect('/log-in')
+app.post('/sign-up', async (req, res, next) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        await db.addUser(req.body.username, hashedPassword)
+        res.redirect('/log-in')
+    } catch(err) {
+        console.log(err)
+        next(err)
+    }
 })
 
 app.post('/log-in', (req, res) => {
@@ -26,6 +35,11 @@ app.get('/log-in', (req, res) => {
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!'); 
+});
 
 app.listen(process.env.PORT, () => {
     console.log('App successfully running')
