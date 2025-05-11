9901ba4742855d4c789ec29e55bf8dea3076e8c7
Punith1117 <punith11172006@gmail.com>
2025-05-11 14:43:28 +0530

    add log-in and sign-up views and routes for get and post requests for each

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.post('/sign-up', (req, res) => {
+    res.redirect('/log-in')
+})
+
+app.post('/log-in', (req, res) => {
+    res.redirect('/')
+})
+
+app.get('/sign-up', (req, res) => {
+    res.render('sign-up')
+})
+
+app.get('/log-in', (req, res) => {
+    res.render('log-in')
+})
+
 app.get('/', (req, res) => {
     res.render('index')
 })
