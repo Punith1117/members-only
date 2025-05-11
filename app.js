0b11d5f16d525806377fa1d9d75d54a1b14d6a34
Punith1117 <punith11172006@gmail.com>
2025-05-11 14:56:08 +0530

    add add to use req.body and install bcryptjs package
    bcryptjs package is used to hash the password with salting

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('node:path')
 const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 app.post('/sign-up', (req, res) => {
     res.redirect('/log-in')
