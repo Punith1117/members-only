7b991ae63348436cc9291614e8d67ddf3f61f675
Punith1117 <punith11172006@gmail.com>
2025-05-14 20:34:21 +0530

    add function to check if the user is admin

--- middleware.js
+++ middleware.js
@@ -6,6 +6,17 @@ function isAuthenticated(req, res, next) {
     }
 }
 
+function isAdmin(req, res, next) {
+    if (req.isAuthenticated()) {
+        if (req.user.role == 'Admin') {
+            next()
+        } else 
+            res.status(403).send('You are not authorized')
+    } else
+        res.status(401).send('You are not authenticated')
+}
+
 module.exports = {
-    isAuthenticated
+    isAuthenticated,
+    isAdmin
 }
