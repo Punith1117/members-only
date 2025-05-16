3a8c6e8c6ba0ffec098e8222535a1f7dbca3712d
Punith1117 <punith11172006@gmail.com>
2025-05-16 18:19:53 +0530

    add code to enable using static files in public folder

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ const db = require('./db/queries')
 const path = require('node:path')
 const { body, validationResult, matchedData } = require('express-validator')
 const { isAuthenticated, isAdmin } = require('./middleware')
+const assetsPath = path.join(__dirname, "public");
 
 const app = express()
 dayjs.extend(utc)
 dayjs.extend(timezone)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.static(assetsPath));
 app.use(express.urlencoded({extended: true}))
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false}))
 app.use(passport.session())
